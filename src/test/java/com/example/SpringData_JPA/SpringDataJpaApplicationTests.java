9e692a82be816b5b126f7db5dd35bda4c660315c
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-07-26 17:45:16 +0330

    write testBookRepository test

--- src/test/java/com/example/SpringData_JPA/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/SpringData_JPA/SpringDataJpaApplicationTests.java
@@ -1,10 +1,22 @@
 package com.example.SpringData_JPA;
 
+import com.example.SpringData_JPA.repositories.BookRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void testBookRepository() {
+		long count = bookRepository.count();
+		Assertions.assertThat(count).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
