40a48ab3f6a6f2f9230d5f3fa6ea52ad560db5aa
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-07-27 14:09:13 +0330

    Schema Initialization with Hibernate

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+drop table if exists book;
+drop table if exists book_sequence;
+
+create table book (
+        id bigint not null auto_increment,
+        isbn varchar(255),
+        publisher varchar(255),
+        title varchar(255),
+        primary key (id)
+) engine=InnoDB;
+
+-- in Hibernate 6 use book_seq (<table_name>_seq) NOT hibernate_sequence
+create table book_sequence (
+    next_val bigint
+)engine=InnoDB;
+
+insert into book_sequence values (1);
