15315d62dad8152ad25169e51f8343192682f75d
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-08-01 23:09:47 +0330

    add Author entity

---
+++ src/main/java/com/example/SpringData_JPA/domain/Author.java
@@ -0,0 +1,36 @@
+package com.example.SpringData_JPA.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
